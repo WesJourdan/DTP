a0fc7f2e9307f2df442dba0dcc75d46a2f2d65c8
Wes Jourdan <wes.jourdan@icloud.com>
2018-02-15 11:54:37 -0500

    Still updating the fetchImage function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,19 @@
 import axios from "axios";
-import googleApiKey from "../private/api-keys.js";
-import cx from "../private/api-keys.js";
+// import {googleApiKey, cx} from "../private";
+
+
 
 export const FETCH_IMAGE = "fetch_image";
 
 const ROOT_URL = "https://www.googleapis.com/customsearch/v1?";
 
 export function fetchImage(searchTerm) {
-  const request = axios.get(`${ROOT_URL}key=${googleApiKey}&cx=${cx}&q=${searchTerm}`);
+  const request = axios.get(`${ROOT_URL}key=${googleApiKey}&${cx}&q=${searchTerm}&searchType=image`);
+  debugger;
   return {
     type: FETCH_IMAGE,
     payload: request
   };
 }
+
+fetchImage('cat')
