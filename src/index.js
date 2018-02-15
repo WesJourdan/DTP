174b08436a22976faac9df02e0493f8584254845
Megan Honig <meganhonig@gmail.com>
2018-02-15 11:47:45 -0500

    add game-board

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promise from "redux-promise";
+import reducers from "./reducers";
+import GameBoard from "./components/game-board";
+import 'bootstrap/dist/css/bootstrap.css';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+const App = () => {
+  return  <GameBoard />
+  // (
+  //   // <Provider store={createStoreWithMiddleware(reducers)}>
+  //     {/* <SearchBar /> */}
+  //
+  //   {/* </Provider> */}
+  // );
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
