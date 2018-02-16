21364368eba69d92277b746bb27ec654b978a682
Wes Jourdan <wes.jourdan@icloud.com>
2018-02-15 22:33:36 -0500

    In the middle of wiring the image reducer.

--- src/reducers/reducer-images.js
+++ src/reducers/reducer-images.js
@@ -2,11 +2,21 @@ import _ from "lodash";
 import {FETCH_IMAGE} from "../actions";
 
 
-export default function(state = {}, action) {
+export default function(state = {steps: []}, action) {
   switch (action.type) {
     case FETCH_IMAGE:
-    debugger;
-      return { ...state, [action.payload.data.id]: action.payload.data };
+      let imageArray = action.payload.data.items
+
+      for (let i = 0; i < imageArray.length; i++ ) {
+        console.log("in loop");
+        let image = imageArray[i];
+        if (image.mime == "image/jpeg") {
+          console.log(image.link);
+          //figure out if this is exiting the loop when the return statement executes (I think it is...)
+          debugger;
+          return { ...state.steps, url: image.link, type: "image"}
+        }
+      };
     default:
       return state;
   }
