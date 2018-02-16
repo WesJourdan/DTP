33dc3d69b1dbe29507e1d442f74c692340c8bd33
Wes Jourdan <wes.jourdan@icloud.com>
2018-02-16 11:11:06 -0500

    combined reducers and structured state

---
+++ src/reducers/reducer-steps.js
@@ -0,0 +1,31 @@
+import _ from "lodash";
+import {FETCH_IMAGE} from "../actions";
+import {FETCH_CAPTION} from "../actions";
+
+
+export default function(state = [], action) {
+
+  switch (action.type) {
+    case FETCH_IMAGE:
+      let imageArray = action.payload.data.items
+      for (let i = 0; i < imageArray.length; i++ ) {
+        console.log("in loop");
+        let image = imageArray[i];
+        if (image.mime == "image/jpeg") {
+          let newSteps = [...state, {url: image.link, type: "image"}]
+          console.log(newSteps);
+          return newSteps;
+        }
+      };
+    case FETCH_CAPTION:
+      let caption = action.payload.data.description.captions[0].text;
+      console.log(caption);
+      if (caption) {
+        let newSteps = [...state.steps, {text: caption, type: "caption"}]
+        console.log({...state, steps: newSteps});
+        return {...state, steps: newSteps};
+      }
+    default:
+      return state;
+  }
+}
